7c7143141b4ccf25f22c8014821267372604b2aa
PhornKhouch <97729971+PhornKhouch@users.noreply.github.com>
2025-04-20 21:15:38 +0700

    update file
    create

--- action/StaffProfile/create.php
+++ action/StaffProfile/create.php
@@ -24,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'startDate' => 'Start Date',
             'status' => 'Status'
         ];
+ 
 
         $missingFields = [];
         foreach ($requiredFields as $field => $label) {
